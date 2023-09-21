087fbc6d319155d5e5003ed508b256f378064355
Hanna Straathof <hanna.straathof@yahoo.com>
2023-09-21 14:50:15 +0200

    constructor voor Token gemaakt

--- src/Token.java
+++ src/Token.java
@@ -1,5 +1,5 @@
 public class Token {
-    enum Type {
+    enum Options {
         PAROPEN,
         PARCLOSE,
         LAMBDA,
@@ -8,5 +8,36 @@ public class Token {
         DOT  // MISSCHIEN DEZE WEGHALEN
     }
 
+    Options type;
     String value;
+
+    public Token(String newValue) {
+        switch(newValue) {
+            case " ":
+                type = Options.SPACE;
+                this.value = newValue;
+                System.out.println("Token constructor space: " + this.value);
+                break;
+            case "(":
+                type = Options.PAROPEN;
+                this.value = newValue;
+                System.out.println("Token constructor paropen: " + this.value);
+                break;
+            case ")":
+                type = Options.PARCLOSE;
+                this.value = newValue;
+                System.out.println("Token constructor parclose: " + this.value);
+                break;
+            case "\\":
+                type = Options.LAMBDA;
+                this.value = newValue;
+                System.out.println("Token constructor lambda: " + this.value);
+                break;
+            default:
+                type = Options.VAR;
+                this.value = newValue;
+                System.out.println("Token constructor var: " + this.value);
+                break;
+        }
+    }
 }
