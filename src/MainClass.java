1b2c2eb6a0b2912034112453b2e6ef9e8efcfea0
Hanna Straathof <hanna.straathof@yahoo.com>
2023-09-21 14:49:33 +0200

    leesin functie gemaakt

--- src/MainClass.java
+++ src/MainClass.java
@@ -2,13 +2,54 @@ import java.util.Scanner;
 
 public class MainClass {
 
+    public static boolean leesIn(String invoer, Parser parser) {
+        boolean isVar = false;
+        String var = "";
+        
+        for(int i = 0; i < invoer.length(); i++) {
+            System.out.println("leesIn: " + invoer.charAt(i));
+            if(invoer.charAt(i) == ' ' || invoer.charAt(i) == '\\' || invoer.charAt(i) == '(' || invoer.charAt(i) == ')') {
+                if(isVar) {
+                    Token nieuwVarToken = new Token(var);
+                    parser.addToken(nieuwVarToken);
+                }
+                Token nieuw  = new Token(invoer.substring(i, i + 1));
+                parser.addToken(nieuw);
+                isVar = false;
+            }
+
+            else if((invoer.charAt(i) >= '0' && invoer.charAt(i) <= '9' ) || (invoer.charAt(i) >= 'a' && invoer.charAt(i) <= 'z' ) || (invoer.charAt(i) >= 'A' && invoer.charAt(i) <= 'Z')) {
+                isVar = true;
+                var = var.concat(invoer.substring(i, i + 1));
+            }
+
+            else {
+                System.out.println("Dit werkt niet loser");
+                return false;
+                
+            }
+
+        }
+
+        if(isVar) {
+            Token nieuwVarToken = new Token(var);
+            parser.addToken(nieuwVarToken);
+        }
+        System.out.println("Yes dit werkt");
+        return true;
+        
+    }
+    
     public static void main(String[] args) {
+        Parser parser = new Parser();
         System.out.println("Please enter a string:");
 
         Scanner sc = new Scanner(System.in);
         String string = sc.nextLine();
+        leesIn(string, parser);
         sc.close();
         System.out.println(string);
-
+        
     }
+    
 }
