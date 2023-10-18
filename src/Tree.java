c428d5c82aa27dc9909dc8f5aaad99842f1ba1e0
Hanna Straathof <hanna.straathof@yahoo.com>
2023-10-18 11:22:30 +0200

    added functions for tree structure

--- src/Tree.java
+++ src/Tree.java
@@ -4,4 +4,49 @@ public class Tree {
     public Tree() {
         root = null;
     }
+
+    public Node getRoot() {
+        return root;
+    }
+
+    // Returns a node which still has availability for children
+    public boolean findGap(Node node, Node gapNode) {
+        if(node == null) {
+            gapNode = node;
+            return true;
+        }
+
+        if(findGap(node.leftChild, gapNode)) {
+            return true;
+        }
+
+        if((node.token.isLambda() || node.token.isApply()) && (node.leftChild == null || node.rightChild == null)) {
+            gapNode = node;
+            return true;
+        }
+
+        return (findGap(node.rightChild, gapNode));
+    }
+
+    // Adds a node (newnode) to the left or right child of a parent node (gapNode)
+    public boolean addNode(Node newNode, Node gapNode) {
+        if(gapNode == null) {
+            root = newNode;
+            return true;
+        }
+        else {
+            if(gapNode.leftChild == null) {
+                gapNode.leftChild = newNode;
+                return true;
+            }
+            else if(gapNode.rightChild == null) {
+                gapNode.rightChild = newNode;
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
 }
+
