aa963a17cc7448e75a55ae4ab51cd146577fb424
Domen <domensmart@gmail.com>
2023-12-14 16:32:43 +0100

    meerdere expressies inlezen

--- Assignment_2/src/MainClass.java
+++ Assignment_2/src/MainClass.java
@@ -1,3 +1,5 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class MainClass {
@@ -50,22 +52,59 @@ public class MainClass {
         }
         // System.out.println("Yes dit werkt");
         return true;
-        
     }
+
+
+    private static String LeesFileExpressie(String filenaam) {
+         
+       try{
+          Scanner filescanner = new Scanner(new File(filenaam)); // We initialize a new scanner
+          StringBuilder expressie = new StringBuilder(); // We initialize a new stringbuilder
+
+          while(filescanner.hasNextLine()){
+             expressie.append(filescanner.nextLine()); // We add the next line to the stringbuilder
+             expressie.append("_");
+                //System.out.println(expressie.toString());
+          }
+          filescanner.close(); // We close the scanner
+          return expressie.toString(); // We return the stringbuilder as a string
+       }
+       catch(FileNotFoundException e){
+          System.err.println("File not found");
+          return null;
+       }
+     }
+
     
     public static void main(String[] args) {
        
       // Make it so that the user can input multiple expressions
-        ParserOp2 parser = new ParserOp2(); // We initialize a new object parser
+        int index = 0; //Iterator which loops over the substrings, so you can read in multiple expressions
+        int laatste_expressie = 0; //Keeps track of where the last expression has been built
         // System.out.println("Please enter a string:");
         int exitStatus = 1;
         Scanner sc = new Scanner(System.in); // We initialize a new scanner
         String string = sc.nextLine(); // We read the input of the user
+        String alle_expressies = LeesFileExpressie(string);
         
-        if(leesIn(string, parser)) {
-            exitStatus = parser.parse();
+        System.out.println("Uiteindelijke: ");
+        System.out.println(alle_expressies);
+        
+        while(laatste_expressie < alle_expressies.length()){ // Check voor einde expressies
+            ParserOp2 parser = new ParserOp2(); // We initialize a new object parser
+            String expressie = ""; 
+            index++;
+            while(index < alle_expressies.length() && alle_expressies.charAt(index) != '_') { //Zolang geen spatie, maak de substring verder
+               index++;
+            }
+            expressie = alle_expressies.substring(laatste_expressie, index); // Maak de substring
+            System.out.println("Substring:");
+            System.out.println(expressie);
+            if(leesIn(expressie, parser)) {
+               exitStatus = parser.parse();
+            }
+            laatste_expressie = index + 1;
         }
-
         sc.close(); // We close the scanner
         // System.out.println(exitStatus);
 
